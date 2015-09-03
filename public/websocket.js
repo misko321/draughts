c18948d1eeffb387086ef4fe559f2320dd2d0aa5
Michal Patron <michal.patron@gmail.com>
2015-09-03 14:11:02 +0200

    Client Websocket code refactoring

--- public/websocket.js
+++ public/websocket.js
@@ -1,41 +1,72 @@
 var Websocket = function(url) {
   this.url = url;
-  client = io.connect(url);
+  this.client = io.connect(url);
 
-  client.on('connect', function(socket) {
-    console.log('Trying to connect...');
-    client.on('connect-ack', function(msg) {
-      console.log('Success: ' + msg.message);
+  var ws = this;
+  this.client.on('connect', function(socket) {
+
+    ws.connect();
+    ws.joinGame();
+
+    ws.client.on('move', function(msg) {
+      ws.applyMove(msg);
     });
 
-    var token = UrlManager.getToken();
-    if (token === undefined) {
-      client.emit('join-new-game', null);
-      client.on('join-new-game-ack', function(msg) {
-        console.log(msg.status + ': ' + msg.message + ', token: ' + msg.token);
-        board = new Board(msg.tiles);
-        UrlManager.setToken(msg.token);
-        initializeBoard(msg.status, msg.tiles);
-      });
-    } else {
-      client.emit('join-existing-game', { token: token });
-      client.on('join-existing-game-ack', function(msg) {
-        //TODO Logger class?
-        console.log(msg.status + ": " + msg.message + ', token: ' + msg.token);
-        initializeBoard(msg.status, msg.tiles);
-       });
-    }
-
-    client.on('disconnect-ack', function(msg) {
-      console.log('Success: ' + msg.message);
-    });
-
-    client.on('move', function(msg) {
-      console.log(msg);
+    //TODO move to disconnect method
+    ws.client.on('disconnect-ack', function(msg) {
+      ws.disconnectAck(msg);
     });
   });
+  // this.client = client;
+};
 
-  this.client = client;
+Websocket.prototype.connect = function() {
+  console.log('Trying to connect...');
+  this.client.on('connect-ack', function(msg) {
+    console.log(msg.status + ': ' + msg.message);
+  });
+};
+
+Websocket.prototype.joinGame = function() {
+  var token = UrlManager.getToken();
+  if (token === undefined) {
+    this.joinNewGame();
+  } else {
+    this.joinExistingGame(token);
+  }
+};
+
+Websocket.prototype.joinNewGame = function() {
+  this.client.emit('join-new-game', null);
+  this.client.on('join-new-game-ack', function(msg) {
+    console.log(msg.status + ': ' + msg.message + ', token: ' + msg.token);
+    board = new Board(msg.tiles);
+    UrlManager.setToken(msg.token);
+    initializeBoard(msg.status, msg.tiles);
+  });
+};
+
+Websocket.prototype.joinExistingGame = function(token) {
+  this.client.emit('join-existing-game', {
+    token: token
+  });
+  this.client.on('join-existing-game-ack', function(msg) {
+    //TODO Logger class?
+    console.log(msg.status + ": " + msg.message + ', token: ' + msg.token);
+    initializeBoard(msg.status, msg.tiles);
+  });
+};
+
+Websocket.prototype.disconnect = function (msg) {
+  console.log('Success: ' + msg.message);
+};
+
+Websocket.prototype.disconnectAck = function (msg) {
+  console.log('Success: ' + msg.message);
+};
+
+Websocket.prototype.applyMove = function(msg) {
+  console.log('move');
 };
 
 Websocket.prototype.emit = function(tileFrom, tileTo) {
