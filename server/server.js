93351ffc76008be1a71e77e3dbcf098b4c9866a9
Michal Patron <michal.patron@gmail.com>
2015-09-07 10:59:42 +0200

    Refactor server/server.js

--- server/server.js
+++ server/server.js
@@ -28,94 +28,114 @@ var lastFreeToken;
 var games = [];
 
 io.on('connection', function(socket) {
+
+  connect(socket);
+
+  //FIXME: throw everything to separate functions +REFACTOR
+  socket.on('join-new-game', function(msg) {
+    joinNewGame(socket, msg);
+  });
+
+  socket.on('join-existing-game', function(msg) {
+    joinExistingGame(socket, msg);
+  });
+
+  socket.on('move', function(msg) {
+    move(socket, msg);
+  });
+
+  //TODO notify the other player if connection with the first one is broken +STD_FEATURE
+  socket.on('disconnect', function(msg) {
+    disconnect(socket, msg);
+  });
+
+});
+
+function connect(socket) {
   console.log('A new User connected');
   socket.emit('connect-ack', {
     status: 'OK',
     message: 'You have successfully connected to the server'
   });
+}
 
-  //FIXME: throw everything to separate functions +REFACTOR
-  socket.on('join-new-game', function(msg) {
-    var game;
-    var player = new Player(msg.username, socket);
+function joinNewGame(socket, msg) {
+  var game;
+  var player = new Player(msg.username, socket);
+  socket.player = player;
 
-    if (!someoneWaitsInQueue) {
-      lastFreeToken = randomstring.generate();
-      game = new Game(lastFreeToken);
-      games[lastFreeToken] = game;
-      game.join(player);
-      someoneWaitsInQueue = true; //I'm waiting
-    } else {
-      game = games[lastFreeToken];
-      game.join(player);
-      someoneWaitsInQueue = false;
-    }
+  if (!someoneWaitsInQueue) {
+    lastFreeToken = randomstring.generate();
+    game = new Game(lastFreeToken);
+    games[lastFreeToken] = game;
+    game.join(player);
+    someoneWaitsInQueue = true; //I'm waiting
+  } else {
+    game = games[lastFreeToken];
+    game.join(player);
+    someoneWaitsInQueue = false;
+  }
 
-    socket.join(lastFreeToken);
-    socket.emit('join-new-game-ack', {
+  socket.join(lastFreeToken);
+  socket.emit('join-new-game-ack', {
+    status: 'OK',
+    message: 'A new game was created',
+    token: lastFreeToken,
+    tiles: game.tiles
+  });
+
+  //if two players joined and game can be started
+  if (someoneWaitsInQueue === false) {
+    game.start();
+    sendSecondPlayerJoins(game.players[0], game.players[1].username);
+    sendSecondPlayerJoins(game.players[1], game.players[0].username);
+  }
+
+  function sendSecondPlayerJoins(player, opponentUsername) {
+    player.socket.emit('second-player-joins', {
       status: 'OK',
-      message: 'A new game was created',
-      token: lastFreeToken,
+      message: 'Your opponent has joined the game',
+      color: player.color,
+      "username": opponentUsername
+    });
+  }
+}
+
+function joinExistingGame(socket, msg) {
+  var game = games[msg.token];
+  if (game) {
+    var playersCount = game.rejoin(); //TODO would be more reliable if white/blackPresent was used +RETHINK
+    socket.emit('join-existing-game-ack', {
+      status: 'OK',
+      message: 'You have successfully rejoined the game',
+      token: msg.token,
       tiles: game.tiles
     });
-
-    //if two players joined and game can be started
-    if (someoneWaitsInQueue === false) {
-      game.start();
-      sendSecondPlayerJoins(game.players[0], game.players[1].username);
-      sendSecondPlayerJoins(game.players[1], game.players[0].username);
-    }
-
-    function sendSecondPlayerJoins(player, opponentUsername) {
-      player.socket.emit('second-player-joins', {
-        status: 'OK',
-        message: 'Your opponent has joined the game',
-        color: player.color,
-        "username": opponentUsername
-      });
-    }
-  });
-
-  socket.on('join-existing-game', function(msg) {
-    var game = games[msg.token];
-    if (game) {
-      var playersCount = game.rejoin(); //TODO would be more reliable if white/blackPresent was used +RETHINK
-      socket.emit('join-existing-game-ack', {
-        status: 'OK',
-        message: 'You have successfully rejoined the game',
-        token: msg.token,
-        tiles: game.tiles
-      });
-    } else {
-      socket.emit('join-existing-game-ack', {
-        status: 'ERROR',
-        message: 'A game with given token does not exist'
-      });
-    }
-  });
-
-  socket.on('move', function(msg) {
-    var game = games[msg.token];
-    game.makeMove(msg.move);
-    io.to(msg.token).emit('move', {
-      status: 'OK',
-      move: msg.move
+  } else {
+    socket.emit('join-existing-game-ack', {
+      status: 'ERROR',
+      message: 'A game with given token does not exist'
     });
-  });
+  }
+}
 
-  //TODO notify the other player if connection with the first one is broken +STD_FEATURE
-  socket.on('disconnect', function() {
-    socket.emit('disconnect-ack', {
-      status: 'OK',
-      message: 'You have been disconnected from the server'
-    });
-    console.log('user disconnected');
+function move(socket, msg) {
+  var game = games[msg.token];
+  game.makeMove(msg.move);
+  io.to(msg.token).emit('move', {
+    status: 'OK',
+    move: msg.move
   });
+}
 
-  // socket.on('move', function(msg) {
-  //   io.emit('move', msg);
-  // });
-});
+function disconnect(socket, msg) {
+  
+  socket.emit('disconnect-ack', {
+    status: 'OK',
+    message: 'You have been disconnected from the server'
+  });
+  console.log('user disconnected');
+}
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
