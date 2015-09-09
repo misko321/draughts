d8c4996e642a5a149dcbebfa86f1a24bf8f22bbd
Michal Patron <michal.patron@gmail.com>
2015-09-09 16:01:06 +0200

    Restored standard draughts board's men layout

--- server/game.js
+++ server/game.js
@@ -7,36 +7,36 @@ var Game = function(token) {
   this.turn = 'W';
   this.lastMoveWasBeat = undefined;
 
-  // for (var i = 0; i < Game.tilesCount; ++i) {
-  //   this.tiles[i] = [];
-  //   for (var j = 0; j < Game.tilesCount; ++j) {
-  //     if ((i + j) % 2 === 0)
-  //       this.tiles[i][j] = 'N';
-  //     else if (j < 4)
-  //       this.tiles[i][j] = 'B';
-  //     else if (j < 6)
-  //       this.tiles[i][j] = 'E';
-  //     else {
-  //       this.tiles[i][j] = 'W';
-  //     }
-  //   }
-  // }
-
-  //FOR QUICK TESTING:
   for (var i = 0; i < Game.tilesCount; ++i) {
     this.tiles[i] = [];
     for (var j = 0; j < Game.tilesCount; ++j) {
       if ((i + j) % 2 === 0)
         this.tiles[i][j] = 'N';
-      else if (j === 2 || j === 4 || j === 0)
+      else if (j < 4)
         this.tiles[i][j] = 'B';
-      else if (j === 5 || j === 7 || j === 9)
-        this.tiles[i][j] = 'W';
-      else {
+      else if (j < 6)
         this.tiles[i][j] = 'E';
+      else {
+        this.tiles[i][j] = 'W';
       }
     }
   }
+
+  //FOR QUICK TESTING:
+  // for (var i = 0; i < Game.tilesCount; ++i) {
+  //   this.tiles[i] = [];
+  //   for (var j = 0; j < Game.tilesCount; ++j) {
+  //     if ((i + j) % 2 === 0)
+  //       this.tiles[i][j] = 'N';
+  //     else if (j === 2 || j === 4 || j === 0)
+  //       this.tiles[i][j] = 'B';
+  //     else if (j === 5 || j === 7 || j === 9)
+  //       this.tiles[i][j] = 'W';
+  //     else {
+  //       this.tiles[i][j] = 'E';
+  //     }
+  //   }
+  // }
 };
 
 Game.tilesCount = 10;
